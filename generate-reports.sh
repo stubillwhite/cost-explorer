66fce0d06b9846056537eed8df3660f4810e0bfc
Stu W <s.white.1@elsevier.com>
2025-01-03 10:34:59 +0000

    Initial version

---
+++ generate-reports.sh
@@ -0,0 +1,38 @@
+function generate-report() {
+    local account="$1"
+    local env="$2"
+
+    echo "--------------------------------------------------------------------------------"
+    echo "-- Generating report for ${account} ${env}"
+    echo "--------------------------------------------------------------------------------"
+    echo
+
+    aws-login "${account}" "${env}"
+    echo
+
+    make run
+    mv "cost-explorer-report.xlsx" "reports/${account}-${env}-cost-explorer-report.xlsx"
+}
+
+mkdir -p reports
+
+generate-report agdatascience non-prod
+generate-report bos           non-prod
+generate-report bos           prod
+generate-report cef           embase
+generate-report cef           prod
+generate-report dkp           non-prod
+generate-report dkp           prod
+# generate-report recs          dev
+# generate-report recs          prod
+generate-report sc-content    non-prod
+generate-report sc-content    prod
+generate-report sd-content    non-prod
+generate-report sd-content    prod
+
+# generate-report cef           backup
+# generate-report cef           candi
+# generate-report scopus-search non-prod
+# generate-report scopus-search prod
+
+open reports
--- src/cost_explorer/app.py
+++ src/cost_explorer/app.py
@@ -7,22 +7,38 @@ import boto3
 from dateutil.relativedelta import relativedelta
 from pandas import DataFrame, ExcelWriter
 
-# Constants
-MONTHS_TO_REPORT = 6
-FILENAME = "cost_explorer_report_white.xlsx"
+MONTHS_TO_REPORT = 12
+FILENAME = "cost-explorer-report.xlsx"
+CHART_COLUMN = "O"
+CHART_HEIGHT = 42
+
 
 class DataType(Enum):
     Total = auto()
     Delta = auto()
 
 
+class Chart(Enum):
+    Timeline = auto()
+    PieChartOfLatest = auto()
+    TimelineByCategory = auto()
+
+
 @dataclass
 class Report:
     title: str
     data: DataFrame
+    charts: List[Chart]
+    fmt_label: Callable[[str], str]
 
 
-def _create_report(title: str, group_by: List[Dict[str, str]] = [], data_type: DataType = DataType.Total) -> Report:
+def _create_report(
+    title: str,
+    group_by: List[Dict[str, str]] = [],
+    data_type: DataType = DataType.Total,
+    charts: List[Chart] = [Chart.Timeline],
+    fmt_label: Callable[[str], str] = lambda x: x,
+) -> Report:
 
     client = boto3.client("ce", region_name="us-east-1")
     start = (datetime.date.today() - relativedelta(months=+MONTHS_TO_REPORT)).replace(day=1)
@@ -42,9 +58,9 @@ def _create_report(title: str, group_by: List[Dict[str, str]] = [], data_type: D
     )
 
     data = [result for response in responses for result in response["ResultsByTime"]]
-    df = _create_dataframe(data, data_type)
+    df = _create_dataframe(data, data_type, fmt_label)
 
-    return Report(title, df)
+    return Report(title, df, charts, fmt_label)
 
 
 def _paginate(f: Callable[[Any], Any], config: Dict[str, Any]) -> Iterable[Dict[str, Any]]:
@@ -58,7 +74,7 @@ def _paginate(f: Callable[[Any], Any], config: Dict[str, Any]) -> Iterable[Dict[
             return None
 
 
-def _create_dataframe(data: List[Dict[str, Any]], data_type: DataType) -> DataFrame:
+def _create_dataframe(data: List[Dict[str, Any]], data_type: DataType, fmt_label: Callable[[str], str]) -> DataFrame:
     rows = []
     sort = ""
     for d in data:
@@ -70,7 +86,7 @@ def _create_dataframe(data: List[Dict[str, Any]], data_type: DataType) -> DataFr
 
         else:
             for group in d["Groups"]:
-                key = group["Keys"][0]
+                key = fmt_label(group["Keys"][0])
                 row[key] = float(group["Metrics"]["UnblendedCost"]["Amount"])
 
         rows.append(row)
@@ -89,21 +105,24 @@ def _create_dataframe(data: List[Dict[str, Any]], data_type: DataType) -> DataFr
 
 
 def _calculate_deltas(df: DataFrame) -> DataFrame:
-    # TODO: Clean up
     deltas = df.copy()
     prev_idx = None
+
     for idx, row in df.iterrows():
-        if prev_idx:
-            for i in row.index:
+        for i in row.index:
+            if prev_idx:
                 deltas.at[idx, i] = df.at[idx, i] - df.at[prev_idx, i]
+            else:
+                deltas.at[idx, i] = 0
         prev_idx = idx
 
-    print(deltas)
     return deltas
 
 
+# TODO: Conditional formatting
+# TODO: Function to transform names (for SubProduct$value labels)
 def _export_reports(reports: List[Report]) -> None:
-    fnam = FILENAME 
+    fnam = FILENAME
 
     print(f"Creating {fnam}")
 
@@ -116,36 +135,91 @@ def _export_reports(reports: List[Report]) -> None:
             report.data.to_excel(writer, sheet_name=report.title)
             worksheet = writer.sheets[report.title]
 
-            chart = workbook.add_chart({"type": "column", "subtype": "stacked"})
+            _insert_charts(workbook, worksheet, report)
 
-            for row_num in range(1, len(report.data) + 1):
-                chart.add_series(
-                    {
-                        "name": [report.title, row_num, 0],
-                        "categories": [report.title, 0, 1, 0, MONTHS_TO_REPORT],
-                        "values": [report.title, row_num, 1, row_num, MONTHS_TO_REPORT],
-                    }
-                )
+            # (max_row, max_col) = report.data.shape
+            # worksheet.conditional_format(1, 1, max_row, max_col, {"type": "3_color_scale"})
 
-            chart.set_y_axis({"label_position": "low"})
-            chart.set_x_axis({"label_position": "low"})
-            worksheet.insert_chart("O2", chart, {"x_scale": 2.0, "y_scale": 2.0})
+
+def _insert_charts(workbook, worksheet, report: Report) -> None:
+
+    chart_row = 2
+
+    # Timeline
+    if Chart.Timeline in report.charts:
+        timeline_chart = workbook.add_chart({"type": "column", "subtype": "stacked"})
+
+        for row_num in range(1, len(report.data) + 1):
+            timeline_chart.add_series(
+                {
+                    "name": [report.title, row_num, 0],
+                    "categories": [report.title, 0, 1, 0, MONTHS_TO_REPORT],
+                    "values": [report.title, row_num, 1, row_num, MONTHS_TO_REPORT],
+                }
+            )
+
+        timeline_chart.set_y_axis({"label_position": "low"})
+        timeline_chart.set_x_axis({"label_position": "low"})
+        worksheet.insert_chart(f"{CHART_COLUMN}{chart_row}", timeline_chart, {"x_scale": 2.0, "y_scale": 2.0})
+
+        chart_row += CHART_HEIGHT
+
+    # Timeline with category
+    if Chart.TimelineByCategory in report.charts:
+        line_chart = workbook.add_chart({"type": "line"})
+
+        for row_num in range(1, len(report.data) + 1):
+            line_chart.add_series(
+                {
+                    "name": [report.title, row_num, 0],
+                    "categories": [report.title, 0, 1, 0, MONTHS_TO_REPORT],
+                    "values": [report.title, row_num, 1, row_num, MONTHS_TO_REPORT],
+                }
+            )
+
+        line_chart.set_y_axis({"label_position": "low"})
+        line_chart.set_x_axis({"label_position": "low"})
+        worksheet.insert_chart(f"{CHART_COLUMN}{chart_row}", line_chart, {"x_scale": 2.0, "y_scale": 2.0})
+
+        chart_row += CHART_HEIGHT
+
+    # Pie chart
+    if Chart.PieChartOfLatest in report.charts:
+        pie_chart = workbook.add_chart({"type": "pie"})
+
+        pie_chart.add_series(
+            {
+                "name": [report.title, 0, MONTHS_TO_REPORT],
+                "categories": [report.title, 1, 0, len(report.data), 0],
+                "values": [report.title, 1, MONTHS_TO_REPORT, len(report.data), MONTHS_TO_REPORT],
+            }
+        )
+
+        pie_chart.set_y_axis({"label_position": "low"})
+        pie_chart.set_x_axis({"label_position": "low"})
+        worksheet.insert_chart(f"{CHART_COLUMN}{chart_row}", pie_chart, {"x_scale": 2.0, "y_scale": 2.0})
+
+        chart_row += CHART_HEIGHT
+
+
+def strip_prefix(prefix: str) -> Callable[[str], str]:
+    def strip(s: str) -> str:
+        return s.replace(prefix, "", 1) 
+
+    return strip
 
 
 def main() -> None:
     # fmt: off
     reports = [
-        #  _create_report("Total",            data_type=DataType.Total),
+        _create_report("Total",            data_type=DataType.Total),
         _create_report("TotalChange",      data_type=DataType.Delta),
-        #  _create_report("SubProduct",       group_by=[{"Type": "TAG",       "Key": "SubProduct"}], data_type=DataType.Total),
-        #  _create_report("SubProductChange", group_by=[{"Type": "TAG",       "Key": "SubProduct"}], data_type=DataType.Delta),
-        #  _create_report("Services",         group_by=[{"Type": "DIMENSION", "Key": "SERVICE"}],    data_type=DataType.Total),
-        #  _create_report("ServicesChange",   group_by=[{"Type": "DIMENSION", "Key": "SERVICE"}],    data_type=DataType.Delta),
-        #  _create_report("Regions",          group_by=[{"Type": "DIMENSION", "Key": "REGION"}],     data_type=DataType.Total),
-        #  _create_report("RegionsChange",    group_by=[{"Type": "DIMENSION", "Key": "REGION"}],     data_type=DataType.Delta),
+        _create_report("SubProduct",       group_by=[{"Type": "TAG",       "Key": "SubProduct"}], data_type=DataType.Total, charts=[Chart.PieChartOfLatest], fmt_label=strip_prefix("SubProduct$")),
+        _create_report("Services",         group_by=[{"Type": "DIMENSION", "Key": "SERVICE"}],    data_type=DataType.Total, charts=[Chart.PieChartOfLatest]),
+        _create_report("ServicesChange",   group_by=[{"Type": "DIMENSION", "Key": "SERVICE"}],    data_type=DataType.Delta, charts=[Chart.TimelineByCategory]),
+        _create_report("Regions",          group_by=[{"Type": "DIMENSION", "Key": "REGION"}],     data_type=DataType.Total, charts=[Chart.PieChartOfLatest]),
+        _create_report("RegionsChange",    group_by=[{"Type": "DIMENSION", "Key": "REGION"}],     data_type=DataType.Delta, charts=[Chart.TimelineByCategory]),
     ]
     # fmt: on
 
     _export_reports(reports)
-
-    #  main_handler()
